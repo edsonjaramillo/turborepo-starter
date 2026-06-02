a6909b3f7ca08fcf0e9cd9eb8f561d8c2b8dad8b
Edson Jaramillo <edsonajaramillo@gmail.com>
2026-06-01 19:48:51 -0500

    email lower

--- packages/db/src/schema/users.ts
+++ packages/db/src/schema/users.ts
@@ -1,16 +1,21 @@
-import { pgTable, text } from "drizzle-orm/pg-core";
+import { pgTable, text, uniqueIndex } from "drizzle-orm/pg-core";
 
+import { lower } from "./functions";
 import { createdAt, id, updatedAt } from "./shared";
 
-export const usersTable = pgTable("users", {
-	id,
-	createdAt,
-	updatedAt,
-	firstName: text("first_name").notNull(),
-	lastName: text("last_name").notNull(),
-	email: text().notNull().unique(),
-	password: text().notNull(),
-});
+export const usersTable = pgTable(
+	"users",
+	{
+		id,
+		createdAt,
+		updatedAt,
+		firstName: text("first_name").notNull(),
+		lastName: text("last_name").notNull(),
+		email: text().notNull(),
+		password: text().notNull(),
+	},
+	(table) => [uniqueIndex("emailUniqueIndex").on(lower(table.email))],
+);
 
 export const userColumns = {
 	id: true,
